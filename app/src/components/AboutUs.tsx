e3348fa85722e65d4180880d7fc99b80b240d93e
Jay <112299655+1552537028@users.noreply.github.com>
2026-02-01 20:29:52 +0530

    Update AboutUs.tsx

--- app/src/components/AboutUs.tsx
+++ app/src/components/AboutUs.tsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
     <div className="py-16 px-4 bottom-20 bg-none relative z-10 max-w-7xl mx-auto">
         <center>
       <h1 className="text-4xl mb-10 font-bold text-yellow-600">About Us</h1>
-      <p className="text-2xl text-white font-light max-w-3xl">
+      <p className="text-lg text-white font-light max-w-3xl">
         Golden Pixel Media is a creative digital marketing agency focused on building powerful brands through strategy-driven content and high-quality visuals. We help businesses grow their online presence by combining creativity, technology, and marketing expertise.
 
 From social media management and performance marketing to professional videography, editing, SEO, and web design, we deliver end-to-end digital solutions tailored to each brand’s goals. Our team believes in storytelling that connects, visuals that convert, and strategies that drive real results.
